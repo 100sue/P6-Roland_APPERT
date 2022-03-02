ff9a9063bd7e255f3543f6c1b3810b24a5edb9a5
appert <scoreur@gmail.com>
2022-03-02 15:54:42 +0100

    ajout de middleware

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // Import d'express afin de créer des applis web avec Node.
 // Import de body-parser afin de pouvoir "parser" le body de la requête.
-// Appel au module Express avec sa fonction.
-// Import mongoose afin de faciliter les intéractions avec la base de données de mongoDB.
+// Import de helmet afin de sécuriser les en-tête http de l'application express.
+// Import de Cors
+// Import mongoose afin de faciliter les interactions avec la base de données de mongoDB.
 // Import de path afin de pouvoir travailler avec les chemins des fichiers(module node qui sert à cacher notre addresse Mongo, marche avec dotenv).
 // Import de dotenv afin de protéger les informations de connexion vers la base de données.
-// Import de helmet afin de sécuriser les en-tête http de l'application express.
+// Appel au module Express avec sa fonction.
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
+
+const app = express();
+app.use(cors())
 
 
 // Routes vers l'utilisateur et les sauces :
@@ -50,12 +55,13 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-
 // Configuration des routes d'API :
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
+
+app.use(helmet());
 
 
 // Exportation du module afin de pouvoir le réutiliser :
